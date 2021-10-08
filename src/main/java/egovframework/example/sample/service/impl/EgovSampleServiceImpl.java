06f77fcd5d4b5059545e4052d61d36246577a0d6
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-10-08 22:41:56 +0900

    working with db(mysql/mariadb)

--- src/main/java/egovframework/example/sample/service/impl/EgovSampleServiceImpl.java
+++ src/main/java/egovframework/example/sample/service/impl/EgovSampleServiceImpl.java
@@ -34,17 +34,15 @@ import org.springframework.stereotype.Service;
  * @Class Name : EgovSampleServiceImpl.java
  * @Description : Sample Business Implement Class
  * @Modification Information
- * @
- * @  수정일      수정자              수정내용
- * @ ---------   ---------   -------------------------------
- * @ 2009.03.16           최초생성
+ * @ @ 수정일 수정자 수정내용 @ --------- --------- ------------------------------- @
+ *   2009.03.16 최초생성
  *
  * @author 개발프레임웍크 실행환경 개발팀
  * @since 2009. 03.16
  * @version 1.0
  * @see
  *
- *  Copyright (C) by MOPAS All right reserved.
+ *      Copyright (C) by MOPAS All right reserved.
  */
 
 @Service("sampleService")
@@ -54,11 +52,11 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/** SampleDAO */
 	// TODO ibatis 사용
-	@Resource(name = "sampleDAO")
-	private SampleDAO sampleDAO;
+//	@Resource(name = "sampleDAO")
+//	private SampleDAO sampleDAO;
 	// TODO mybatis 사용
-	//  @Resource(name="sampleMapper")
-	//	private SampleMapper sampleDAO;
+	@Resource(name = "sampleMapper")
+	private SampleMapper sampleDAO;
 
 	/** ID Generation */
 	@Resource(name = "egovIdGnrService")
@@ -66,6 +64,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 등록한다.
+	 * 
 	 * @param vo - 등록할 정보가 담긴 SampleVO
 	 * @return 등록 결과
 	 * @exception Exception
@@ -85,6 +84,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 수정한다.
+	 * 
 	 * @param vo - 수정할 정보가 담긴 SampleVO
 	 * @return void형
 	 * @exception Exception
@@ -96,6 +96,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 삭제한다.
+	 * 
 	 * @param vo - 삭제할 정보가 담긴 SampleVO
 	 * @return void형
 	 * @exception Exception
@@ -107,6 +108,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글을 조회한다.
+	 * 
 	 * @param vo - 조회할 정보가 담긴 SampleVO
 	 * @return 조회한 글
 	 * @exception Exception
@@ -121,6 +123,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 
 	/**
 	 * 글 목록을 조회한다.
+	 * 
 	 * @param searchVO - 조회할 정보가 담긴 VO
 	 * @return 글 목록
 	 * @exception Exception
@@ -131,10 +134,7 @@ public class EgovSampleServiceImpl extends EgovAbstractServiceImpl implements Eg
 	}
 
 	/**
-	 * 글 총 갯수를 조회한다.
-	 * @param searchVO - 조회할 정보가 담긴 VO
-	 * @return 글 총 갯수
-	 * @exception
+	 * 글 총 갯수를 조회한다. @param searchVO - 조회할 정보가 담긴 VO @return 글 총 갯수 @exception
 	 */
 	@Override
 	public int selectSampleListTotCnt(SampleDefaultVO searchVO) {
